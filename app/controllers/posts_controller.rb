4f2cad74db3f0eb21368b663dd6321a45e38f062
saatoshi <ysatoshiy123456789@gmail.com>
2021-04-03 16:36:50 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
-    def index
-      @posts = Post.all 
-    end
 
-  def new 
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
-
+ 
+   # 削除
+   # def new
+   # end
+ 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
-
-  
-  end
-
+ 
+ end
